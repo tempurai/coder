d80877dc74962ee66965ca3efe8d66364334d4c7
lincan@linux <email@lincan.li>
2025-09-02 19:01:36 +0900

    feat: Remove unused components and files to streamline the codebase

--- src/cli/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { ThoughtSection } from './ThoughtSection.js';
-export { PlanSection } from './PlanSection.js';
-export { ActionSection } from './ActionSection.js';
-export { WelcomeScreen } from './WelcomeScreen.js';
-export { ThemeSelector } from './ThemeSelector.js';
-export { DynamicInput } from './DynamicInput.js';
-export { CodePreview } from './CodePreview.js';
